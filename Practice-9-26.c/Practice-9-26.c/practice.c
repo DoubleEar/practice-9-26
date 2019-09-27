8433ff82955d48a4521feb7dffb071d3f628d606
DoubleEar <1638435068@qq.com>
2019-09-27 18:11:10 +0800

    更新代码

--- Practice-9-26.c/Practice-9-26.c/practice.c
+++ Practice-9-26.c/Practice-9-26.c/practice.c
@@ -7,7 +7,7 @@
 //{
 //	printf("****************\n");
 //	printf("**** 1.play ****\n");
-//	printf("**** 2.exit ****\n");
+//	printf("**** 0.exit ****\n");
 //	printf("****************\n");
 //}
 //void game()
@@ -63,4 +63,149 @@
 //}
 
 //写代码可以在整型有序数组中查找想要的数字,找到了返回下标，找不到返回 - 1.（折半查找）
+//
 
+//int main()
+//{
+//	int input = 0;
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int left = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int right = sz - 1;
+//	while (1)
+//	{
+//		printf("请输入你要查找的数：\n");
+//		scanf("%d", &input);
+//		while (left <= right)
+//		{
+//			int mid = (left + right) / 2;
+//			if (input > arr[mid])
+//			{
+//				left = mid + 1;
+//			}
+//			else if (input < arr[mid])
+//			{
+//				right = mid - 1;
+//			}
+//			else
+//			{
+//				printf("找到啦，下标是%d\n", mid);
+//				break;
+//			}
+//		}
+//		//两种情况
+//		if (left > right)
+//		{
+//			printf("没找到诶\n");
+//		}
+//	}
+//	return 0;
+//
+//}
+//封装函数
+int binary_search(int arr[], int input, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+		printf("请输入你要查找的数：\n");
+		scanf("%d", &input);
+		while (left <= right)
+		{
+			int mid = (left + right) / 2;
+			if (input > arr[mid])
+			{
+				left = mid + 1;
+			}
+			else if (input < arr[mid])
+			{
+				right = mid - 1;
+			}
+			else
+			{
+				return mid;
+				break;
+			}
+		}
+		return -1;
+	}
+
+
+
+
+	int main()
+	{
+		int ret = 0;
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int input = 0;
+		int sz = sizeof(arr) / sizeof(arr[0]);
+		while (1)
+		{
+			ret = binary_search(arr, input, sz);
+			if (ret == -1)
+			{
+				printf("没找到诶\n");
+			}
+			else
+			{
+				printf("找到啦，下标是：%d\n", ret);
+			}
+		}
+		return 0;
+
+	}
+
+
+//编写代码模拟三次密码输入的场景。
+//#include<string.h>
+//int main()
+//{
+//	char input[20] = "";
+//	//数组名本身为地址，不用在取地址；
+//	int i = 0;
+//	for (i = 1; i <= 3; i++)
+//	{
+//		printf("请输入密码：\n");
+//		scanf("%s", input);
+//		if (strcmp(input, "123456")==0)
+//		{
+//			printf("登陆成功\n");
+//			break;
+//		}
+//		if (i == 4)
+//		{
+//			printf("退出程序\n");
+//		}
+//		else
+//		{
+//			printf("重新输入\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+//编写一个程序，可以直接收键盘字符，如果是小写字符就输出对应的大写字符，如果是数字不输出。
+//int main()
+//{
+//	char ch = {0};
+//	while (1)
+//	{
+//		 printf("请输入一个字符：\n");
+//		 ch = getchar();
+//         if (ch >= 'a'&&ch <= 'z')
+//		{
+//			printf("%c\n", ch - 32);
+//		}
+//		else if (ch >= 'A'&&ch <= 'Z')
+//		{
+//			printf("%c\n", ch + 32);
+//		}
+//		else
+//		{
+//			
+//		}
+//		ch = getchar();
+//	}
+//		return 0;
+//	
+//}
